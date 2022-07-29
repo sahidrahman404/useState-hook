cca20b6acdd194218196bb861fc8e40024957329
Sahid Rahman <sahidrahman404@gmail.com>
2022-07-29 11:48:46 +0700

    add useState hook to the greeting function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-function App() {}
+function Greeting({ initialName }: { initialName?: string }) {
+  const [name, setName] = useState<string>(initialName ? initialName : "");
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
+
+  return (
+    <div>
+      <form
+        onSubmit={(event: FormEvent<HTMLFormElement>) => event.preventDefault()}
+      >
+        <label htmlFor="name">Name: </label>
+        <input onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : "Please type your name"}
+    </div>
+  );
+}
+
+function App() {
+  return <Greeting />;
+}
 
 export default App;
